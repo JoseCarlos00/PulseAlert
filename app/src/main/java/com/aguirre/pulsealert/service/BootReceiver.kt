fa533ac3efc46b158d80ff094dc1eb8c6745189c
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-05-19 20:06:05 -0600

    Add Module BootReceiver.kt

---
+++ app/src/main/java/com/aguirre/pulsealert/service/BootReceiver.kt
@@ -0,0 +1,15 @@
+package com.aguirre.pulsealert.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, SocketForegroundService::class.java)
+            context.startForegroundService(serviceIntent)
+        }
+    }
+}
