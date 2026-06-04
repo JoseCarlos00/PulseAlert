248acbf5bd9689a9e4c04b012bd0a0a8f73de473
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-06-04 13:57:17 -0600

    Add file release.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 27
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
